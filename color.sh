2729affe6774692435f8e0541d0ac1463441f435
wangzme <wangzme@outlook.com>
2019-06-26 15:54:56 +0800

    commit message

---
+++ color.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cd ~/Repos/CTCW/
+
+# 利用中国传统色创建纯色壁纸,脚本待完成。
+# Create solid color wallpapers with Chinese troditional colors. (unfinished)
+rm -r Hex
+rm -r Name
+rm -r Solid
+mkdir {Hex,Name,Solid}
+
+cat Colors | while read line
+do      
+        typeset -u hex
+        hex=`echo $line|cut -d"," -f2`
+        name=`echo $line|cut -d"," -f1`
+        lname=`LANG=zh_CN.utf-8;echo "$name"|wc -L`
+        echo "|$name| : $lname"
+        pinyin=`echo $line|cut -d"," -f2`
+        file=$pinyin".png"
+        echo "|$file|"
+        hexT=${hex:0:7}
+        (( hexR=0x${hex:1:2} ))
+        (( hexR=0x${hex:3:4} ))
+        (( hexR=0x${hex:5:6} ))
+        
+        
+        fll="#ffffff"
+        if [[ $hexR -ge 130 ]] || [[ $hexG -ge 130 ]] || [[  $hexB -ge 130 ]];then
+                fll="#000000"
+        fi
+        nameT=$name
+        nameT=''
+        for (( i=0;i<$lname/2;i=i+1 ));do
+            nameT=$nameT${name:i:1}\\n
+        done
+        nameT="﹁\n"$nameT"﹂"
+        echo "nameT : $nameT"
+        convert -size 1920x1080 xc:$hexT Solid/$file
+        convert Solid/$file -fill $fll -font "SourceHanSerif-Regular.ttc"\
+                -pointsize 100 \
+                -annotate +1740+100 $nameT\
+                -interline-spacing 100\
+                 -interword-spacing 1\
+                Name/$file
+	echo "添加名字"
+        convert Name/$file -fill $fll -font "monof55.ttf"\
+                -pointsize 60 \
+                -annotate +1500+150 $hex\
+                Hex/$file
+done
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 中国传统色壁纸集
+
+## 随便选了几个
+![](https://github.com/wangzme/CTCW/blob/master/Name/%23f20c00.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%23ff4c00.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%23ffa631.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%23a3d900.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%231685a9.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%232e4e7e.png)
+![](https://github.com/wangzme/CTCW/blob/master/Name/%23574266.png)
